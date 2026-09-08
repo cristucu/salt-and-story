837cbcac401be4c762a29ee74bc727bb0f4540b7
cristucu <cristian.bot23@gmail.com>
2026-09-08 20:08:14 +0300

    feat: add recipe listing and sorting

--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+
 from recipes import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
+    path("recipes/", include("recipes.urls")),
     path("", include("django.contrib.auth.urls")),
+
 ]
 
