dc183ddad250f0c9e2d33f44aaae1bb72c675131
cristucu <cristian.bot23@gmail.com>
2026-09-08 18:05:52 +0300

    feat: add recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,22 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    cooking_time = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="recipes",
+    )
+
+    def __str__(self):
+        return self.title
