71cf8bbb33cf481d091b4160803f9504d8733c6d
cristucu <cristian.bot23@gmail.com>
2026-09-08 21:38:02 +0300

    feat: add recipe editing

--- recipes/urls.py
+++ recipes/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("", views.recipe_list, name="list"),
     path("latest/", views.recipe_latest, name="latest"),
     path("add/", views.recipe_create, name="add"),
+    path("<int:pk>/edit/", views.recipe_update, name="edit"),
     path("<int:pk>/", views.recipe_detail, name="detail"),
 ]
 
